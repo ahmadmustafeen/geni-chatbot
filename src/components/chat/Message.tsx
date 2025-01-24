f988c01bbe176ab4df0f71793f52398bcb3a6dac
H-raheel <h.raheel622@gmail.com>
2025-01-24 17:58:06 +0500

    chat fixed

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -2,12 +2,22 @@ import Image from "next/image";
 import React from "react";
 
 interface MessageProps {
-    message: string;
-    time: string;
-    sender?: string;
-  }
+  message: string;
+  time: string;
+  sender?: string;
+  backgroundColor?: string;
+  textColor?: string;
+  avatar?:string
+}
 
-const Message: React.FC<MessageProps> = ({message,time,sender = "James Johnson"}) => {
+const Message: React.FC<MessageProps> = ({
+  message,
+  time,
+  sender = "James Johnson",
+  backgroundColor = "#eff4fa",
+  textColor = "#6b7280",
+  avatar="/imgs/user-5.jpg"
+}) => {
   return (
     <div className="flex gap-3 mb-8">
       <div className="flex gap-3">
@@ -19,16 +29,24 @@ const Message: React.FC<MessageProps> = ({message,time,sender = "James Johnson"}
             height="48"
             decoding="async"
             className="rounded-full sm:h-12 sm:w-12 h-9 w-9"
-            src="/imgs/user-5.jpg"
+            src={avatar}
             style={{ color: "transparent" }}
           />
         </div>
         <div>
           <div className="text-xs opacity-60 font-medium mb-1 block">
-          {sender}, {time}
+            {sender}, {time}
           </div>
-          <div className="p-2 bg-[#eff4fa] text-gray-500 rounded-md break-words">
-          {message}
+          <div
+            className="p-2 rounded-md "
+            style={{
+              wordBreak: "break-word",
+              hyphens: "auto",
+              backgroundColor: backgroundColor,
+              color: textColor,
+            }}
+          >
+            {message}
           </div>
         </div>
       </div>
--- src/components/chat/Message2.tsx
+++ src/components/chat/Message2.tsx
@@ -3,17 +3,32 @@ import React from "react";
 interface Message2Props {
   message: string;
   time: string;
+  backgroundColor?: string;
+  textColor?: string;
 }
 
-const Message2: React.FC<Message2Props> = ({ message, time }) => {
+const Message2: React.FC<Message2Props> = ({
+  message,
+  time,
+  backgroundColor = "#46caeb25",
+  textColor = "#6b7280",
+}) => {
   return (
-    <div className="flex gap-3 mb-8 sm:max-w-2xl break-words ">
+    <div className="flex gap-3 mb-8 sm:max-w-2xl ">
       <div className="flex justify-end w-full">
         <div>
           <div className="text-xs opacity-60 font-medium mb-1 block text-end">
             {time}
           </div>
-          <div className="p-2 bg-[#46caeb25] text-gray-500 rounded-md">
+          <div
+            className="p-2 rounded-md "
+            style={{
+              wordBreak: "break-word",
+              hyphens: "auto",
+              backgroundColor: backgroundColor,
+              color: textColor,
+            }}
+          >
             {message}
           </div>
         </div>
