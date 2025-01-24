e9ca4473ee6886245b89f5d4d2111d232ef526ce
H-raheel <h.raheel622@gmail.com>
2025-01-24 19:27:39 +0500

    fix2

--- src/components/chat/Message2.tsx
+++ src/components/chat/Message2.tsx
@@ -14,23 +14,21 @@ const Message2: React.FC<Message2Props> = ({
   textColor = "#6b7280",
 }) => {
   return (
-    <div className="flex gap-3 mb-8 sm:max-w-2xl ">
-      <div className="flex justify-end w-full">
-        <div>
-          <div className="text-xs opacity-60 font-medium mb-1 block text-end">
-            {time}
-          </div>
-          <div
-            className="p-2 rounded-md "
-            style={{
-              wordBreak: "break-word",
-              hyphens: "auto",
-              backgroundColor: backgroundColor,
-              color: textColor,
-            }}
-          >
-            {message}
-          </div>
+    <div className="flex justify-end mb-6 sm:max-w-2xl ml-auto">
+      <div className="max-w-[80%]">
+        <div className="text-xs opacity-60 font-medium mb-1 text-end">
+          {time}
+        </div>
+        <div
+          className="p-3 rounded-lg"
+          style={{
+            wordBreak: "break-word",
+            hyphens: "auto",
+            backgroundColor: backgroundColor,
+            color: textColor,
+          }}
+        >
+          {message}
         </div>
       </div>
     </div>
