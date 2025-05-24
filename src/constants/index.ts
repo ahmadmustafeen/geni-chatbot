cbd525334fffc3bb2737711189ee84db433f12ed
Ahmad Mustafeen <engr.ahmadmustafeen@gmail.com>
2025-05-24 14:57:05 +0500

    feat: updated BE url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:4000/api";
+export const BASE_URL = "http://13.61.103.159:4000/api";
+// export const BASE_URL = "http://localhost:4000/api";
 export const ENDPOINTS = {
   chat: "/chat",
   chatbot: "/chatbot",
