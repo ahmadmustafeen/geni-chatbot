66f60e94c7854bf17087c5a4cb444da1fd71a2cc
Ahmad Mustafeen <engr.ahmadmustafeen@gmail.com>
2025-05-24 15:25:59 +0500

    feat: updated backend url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://13.61.103.159:4000/api";
+export const BASE_URL = "https://backend.genimatics.com/api";
 // export const BASE_URL = "http://localhost:4000/api";
 export const ENDPOINTS = {
   chat: "/chat",
