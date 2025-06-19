ff8ca391ac482813f1ea89aaeb8ef17352f1265d
Ahmad Mustafeen <engr.ahmadmustafeen@gmail.com>
2025-06-19 20:32:46 +0500

    feat: added config for iframe

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply to all routes
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL", // Allow rendering in iframe
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *", // Optional: allow all origins to embed (or restrict to your domains)
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
